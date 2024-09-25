c21c5b4f03324f0938b50656c0a53517c9ba56c1
EricJDarnell <eric.j.darnell@gmail.com>
2024-09-25 13:37:50 -0400

    formatting and cleaning up notes a bit

--- index.js
+++ index.js
@@ -8,28 +8,26 @@
 
 //prompt user for a list of flavors seperated by commas.
 const userInputString = prompt(
-  "Please enter your desired froyo-flavors seperated by a comma.",
-  "vanilla,vanilla,strawberry,coffee,vanilla,coffee,chocolate"
+  "Please enter your desired froyo-flavors seperated by a comma."
 );
-// split the string into an array.
-const stringArray = userInputString.split(",");
-// I should sort the array to alphabetical order, to make counting duplicates easier
-const alphaArray = stringArray.sort();
-let n = 0; //default value, just to declare n variable
-const masterArray = []; //declare the master array in which I will hold the formed arrays, to make an array of arrays
+const stringArray = userInputString.split(","); // splits the string into an array.
+
+const alphaArray = stringArray.sort(); // takes inputs and rearranges into alphabetical order
+let n = 0; //declaring n variable, used to count quantity of each flavor ordered
+const masterArray = []; //declare the master array, consisting of several two property arrays
 //yo, I heard you like arrays, so I put an array in your array.
 for (let i = 0; i < alphaArray.length; i++) {
-  const arrayFlavor = alphaArray[i];
+  const arrayFlavor = alphaArray[i]; //declare variable that holds string of flavor
   if (alphaArray[i] === alphaArray[i - 1] || i === 0) {
-    n++;
-  } else {
-    n = 1;
+    n++;  //increases n count whenever the newly iterated flavor is the same as the previous one
+  } else { 
+    n = 1; //resets n count to one if flavor is not repeated
   }
-  const subArray = [arrayFlavor, n]; //creating a two property array for each iteration of i
-  masterArray.push(subArray); // adding each two property array into the master array
+  const subArray = [arrayFlavor, n]; //creates a new two property array for each iteration of i
+  masterArray.push(subArray); // pushes the new two property array into the master array
 }
-// display table of order
-const froyoOrder = Object.fromEntries(masterArray); //mdn: assigning arrays into an object
-// master array had more iterations of pairs, but duplicates were automatically reduced, and count values updated.
-// alphabetizing the entries made that possible.
+const froyoOrder = Object.fromEntries(masterArray); //mdn: creating object from the master array.
+// duplicate key properties are automatically reduced when creating the object from the master array.
+// n count values will be overwritten by the latest duplicate properties.
+// alphabetizing the iteration made this work properly.
 console.table(froyoOrder);
